07189ce77c53cab0ebbb6df5f7df0cdc9871b98e
Vitaly Gromov <vitaly.gro@gmail.com>
2021-10-04 15:02:13 +0300

    Кардинально исправленная версия Homework-adv-4-1

--- src/CallCenter.java
+++ src/CallCenter.java
@@ -1,59 +1,63 @@
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CallCenter {
-    private Queue<String> queue;
-    private boolean working;
-    private volatile static int countAnswers;
+    private final Queue<String> queue;
+    private volatile AtomicBoolean working;
+    private volatile AtomicInteger countAnswers;
 
     private final static int MAX_COUNT_CALLS = 20;
-    private final static int MAX_COUNT_SPECIALIST = 100;
-    private final static int TIME_ATS_SLEEP = 1000;
-    private final static int TIME_SPECIALIST_SLEEP = 4000;
+    private final static int TIME_ATS_SLEEP = 500;
+    private final static int TIME_SPECIALIST_WAIT = 500;
+    private final static int TIME_SPECIALIST_WORKS = 4000;
 
     public CallCenter() {
         queue = new ConcurrentLinkedQueue<String>();
-        working = true;
-        System.out.println("Колл-центр начал работать.");
+        working = new AtomicBoolean(true);
+        countAnswers = new AtomicInteger(0);
     }
 
     public void workOfATS() {
         String bell;
-        try {
-            System.out.println("С АТС начали поступать звонки.");
-            for (int i = 0; i < MAX_COUNT_CALLS; i++) {
-                bell = "Hallo!";
-                System.out.println(bell);
-                queue.add(bell);
-                Thread.sleep(TIME_ATS_SLEEP);
-            }
-            System.out.println("Звонки с АТС закончились.");
+        System.out.println("С АТС начали поступать звонки.");
+        for (int i = 0; i < MAX_COUNT_CALLS; i++) {
+            bell = "Hallo!";
+            System.out.println(bell);
+            queue.add(bell);
 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            try {
+                Thread.sleep(TIME_ATS_SLEEP);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+        System.out.println("Звонки с АТС закончились.");
     }
 
     public void workOfSpecialist() {
+        System.out.println(Thread.currentThread().getName() + " подключился.");
         try {
-            System.out.println(Thread.currentThread().getName() + " подключился.");
-            for (int i = 0; i < MAX_COUNT_SPECIALIST; i++) {
-                if (countAnswers == MAX_COUNT_CALLS) {
-                    working = false;
-                    System.out.println("Колл-центр заканчивает работу работать.");
-                    break;
-                }
+            while (countAnswers.get() < MAX_COUNT_CALLS) {
+                Thread.sleep(TIME_SPECIALIST_WAIT);
 
-                if (queue.size() > 0 && working) {
+                if (queue.size() > 0 && working.get()) {
                     System.out.println(Thread.currentThread().getName() + " принял звонок.");
                     queue.poll();
-                    Thread.sleep(TIME_SPECIALIST_SLEEP);
+
+                    Thread.sleep(TIME_SPECIALIST_WORKS);
+
                     System.out.println(Thread.currentThread().getName() + " ответил на звонок.");
-                    countAnswers++;
+                    countAnswers.getAndIncrement();
                 }
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+
+        if (countAnswers.get() == MAX_COUNT_CALLS) {
+            working.set(false);
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,16 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         CallCenter callCenter = new CallCenter();
+        final int COUNT_SPECIALISTS = 5;
 
         Thread ATS = new Thread(callCenter::workOfATS, "АТС");
 
-        Thread specialist_1 = new Thread(callCenter::workOfSpecialist, "Специалист_1");
-        Thread specialist_2 = new Thread(callCenter::workOfSpecialist, "Специалист_2");
-        Thread specialist_3 = new Thread(callCenter::workOfSpecialist, "Специалист_3");
-        Thread specialist_4 = new Thread(callCenter::workOfSpecialist, "Специалист_4");
-        Thread specialist_5 = new Thread(callCenter::workOfSpecialist, "Специалист_5");
-
         ATS.start();
 
-        specialist_1.start();
-        specialist_2.start();
-        specialist_3.start();
-        specialist_4.start();
-        specialist_5.start();
+        for (int i = 1; i <= COUNT_SPECIALISTS; i++) {
+            String specialist = "Специалист_" + i;
+            new Thread(callCenter::workOfSpecialist, specialist).start();
+        }
 
         ATS.join();
     }
